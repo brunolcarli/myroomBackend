91af3b491f1da01448eb000c6e3dea3829ffe787
brunolcarli <brunolcarli@gmail.com>
2023-09-18 20:44:45 -0300

    ADD base graphq objects and query

--- api/schema.py
+++ api/schema.py
@@ -1,8 +1,91 @@
 import graphene
 from django.conf import settings
+from api.models import UserModel, Room, Article, Photo, ThreadModel, ThreadComment
+
+
+class UserType(graphene.ObjectType):
+    username = graphene.String()
+    avatar = graphene.String()
+    full_name = graphene.String()
+    date_joined = graphene.Date()
+    # room
+
+
+class RoomType(graphene.ObjectType):
+    name = graphene.String()
+    description = graphene.String()
+    user = graphene.Field(UserType)
+    room_picture = graphene.String()
+    background_picture = graphene.String()
+    default_background_active = graphene.Boolean()
+    photos_section_active = graphene.Boolean()
+    articles_section_active = graphene.Boolean()
+    threads_section_active = graphene.Boolean()
+    # photos
+    # author
+    # threads
+
+
+class ArticleType(graphene.ObjectType):
+    room = graphene.Field(RoomType)
+    author = graphene.Field(UserType)
+    title = graphene.String()
+    content = graphene.String()
+    post_datetime = graphene.DateTime
+
+
+class PhotoType(graphene.ObjectType):
+    room = graphene.Field(RoomType)
+    user = graphene.Field(UserType)
+    data = graphene.String()
+    description = graphene.String()
+    public = graphene.Boolean()
+    post_datetime = graphene.DateTime()
+
+
+class ThreadType(graphene.ObjectType):
+    name = graphene.String()
+    content = graphene.String()
+    creation_datetime = graphene.DateTime()
+    author = graphene.Field(UserType)
+    room = graphene.Field(RoomType)
+    last_comment_datetime = graphene.DateTime()
+    public = graphene.Boolean()
+    num_comments = graphene.Int()
+
+
+class ThreadCommentType(graphene.ObjectType):
+    author = graphene.Field(UserType)
+    thread = graphene.Field(ThreadType)
+    post_datetime = graphene.DateTime()
+    content = graphene.String()
 
 
 class Query:
     version = graphene.String()
     def resolve_version(self, info, **kwargs):
         return settings.VERSION
+
+    users = graphene.List(UserType)
+    def resolve_users(self, info, **kwargs):
+        return UserModel.objects.filter(**kwargs)
+
+    rooms = graphene.List(RoomType)
+    def resolve_rooms(self, info, **kwargs):
+        return Room.objects.filter(**kwargs)
+
+    articles = graphene.List(ArticleType)
+    def resolve_articles(self, info, **kwargs):
+        return Article.objects.filter(**kwargs)
+
+    photos = graphene.List(PhotoType)
+    def resolve_photos(self, info, **kwargs):
+        return Photo.objects.filter(**kwargs)
+
+    threads = graphene.List(ThreadType)
+    def resolve_threads(self, info, **kwargs):
+        return ThreadModel.objects.filter(**kwargs)
+
+    thread_comments = graphene.List(ThreadCommentType)
+    def resolve_thread_comments(self, info, **kwargs):
+        return ThreadComment.objects.filter(**kwargs)
