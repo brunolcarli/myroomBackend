59f4b7adfc03ce5d88d61df3e71d1e09ed5781db
brunolcarli <brunolcarli@gmail.com>
2023-09-18 06:49:38 -0300

    ADD database models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserModel(User):
+    avatar = models.FileField(null=True)
+    full_name = models.CharField(max_length=255, null=False, blank=False)
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False, unique=True)
+    description = models.TextField(null=True)
+    user = models.OneToOneField(UserModel, null=True, on_delete=models.CASCADE)
+    room_picture = models.FileField(null=True)
+    background_picture = models.FileField(null=True)
+    default_background_active = models.BooleanField(default=True)
+    photos_section_active = models.BooleanField(default=True)
+    articles_section_active = models.BooleanField(default=True)
+    threads_section_active = models.BooleanField(default=True)
+
+
+class Article(models.Model):
+    room = models.ForeignKey(Room, null=False, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserModel, null=False, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    post_datetime = models.DateTimeField(auto_now_add=True)
+
+
+class Photo(models.Model):
+    room = models.ForeignKey(Room, null=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel, null=False, on_delete=models.CASCADE)
+    data = models.FileField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    public = models.BooleanField(default=True)
+    post_datetime = models.DateTimeField(auto_now_add=True)
+
+
+class ThreadModel(models.Model):
+    name = models.CharField(max_length=128, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(UserModel, null=False, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, null=False, on_delete=models.CASCADE)
+    last_comment_datetime = models.DateTimeField(null=True)
+    public = models.BooleanField(default=True)
+    num_comments = models.IntegerField(default=0)
+
+
+class ThreadComment(models.Model):
+    author = models.ForeignKey(UserModel, null=False, on_delete=models.CASCADE)
+    thread = models.ForeignKey(ThreadModel, null=False, on_delete=models.CASCADE)
+    post_datetime = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(null=False, blank=False)
